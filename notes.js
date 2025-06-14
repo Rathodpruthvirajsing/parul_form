ecb2ac86f0a74bb50179585e66c7738d3e213704
Rathd Pruthviraj <145155770+Rathodpruthvirajsing@users.noreply.github.com>
2025-06-14 12:25:30 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,25 @@
+// package.json  ek configratino  file hai jo batati hai  project me kons kon se packages install hai ,script kya aur project ak absic info
+
+// dependencies
+//  aek project ko chalane me use kiye jane walw sabhi packges ko jaha par mention karne use hi ko depedencies bolte hai
+
+// type of dependencies
+// normal dependencies
+// dev dependencies
+// (npm install parcel -d)\
+
+//  "parcel": "^2.15.2"
+//  2.       15.         2
+//  (major)        (minor)             (patch-fix)
+
+
+// basically patch ko update karna hai to  hum updane project  ke samne ^/~ use karenge  to minor change ho jayeg
+// ^patch cerate
+// ~minor tilde
+
+// package.lock.json
+// this file ate exact verison and record are stored and 
+
+//  ye file exact verrson ka record rakhti  hai  har ek dependecny  aur useke under ki dependacny.
+
+// ye  ensure karta hai ki  har machine pe same verison install ho
